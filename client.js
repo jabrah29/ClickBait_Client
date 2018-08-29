2dd8ea06d231c589a558868312aa6154a97d0411
jacob <jabrah29@uic.edu>
2018-08-28 21:42:01 -0400

    more update

--- client.js
+++ client.js
@@ -1,9 +1,22 @@
 function handleInput() {
-  sendToServer()
+  var title = document.getElementById("inputEmail").value;
+  sendToServer(title)
 }
 document.getElementById('input').onclick = handleInput;
 
 
 function sendToServer(){
-
+  $.ajax({
+  //The URL to process the request
+    'url' : '/titles',
+    'type' : 'GET',
+    'data' : {
+      'title' : title
+    },
+    'success' : function(data) {
+      if (data == "success") {
+        alert('request sent!');
+      }
+    }
+  });
 }
